f314cf0a63dbb6b0133cef45313907d4cc763a3f
Tyler Green <tgreen10@uoguelph.ca>
2020-04-07 16:59:04 -0400

    Azure COmpleted, Data processed, Report Started, Need to calculate Accuracy, Need to finish transcriptions

---
+++ azure-speech-to-text.py
@@ -0,0 +1,50 @@
+import azure.cognitiveservices.speech as speechsdk
+import time
+import wave
+
+def speech_recognize_continuous_from_file(thisisafile):
+    """performs continuous speech recognition with input from an audio file"""
+    # <SpeechContinuousRecognitionWithFile>
+    speech_config = speechsdk.SpeechConfig(subscription="84e47070f07246809f27e90199147298", region="westus")
+    audio_config = speechsdk.audio.AudioConfig(filename=thisisafile)
+
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    done = False
+
+    def stop_cb(evt):
+        """callback that signals to stop continuous recognition upon receiving an event `evt`"""
+        print('CLOSING on {}'.format(evt))
+        nonlocal done
+        done = True
+
+    # Connect callbacks to the events fired by the speech recognizer
+    speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
+    speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
+    speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+    speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+    speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+    # stop continuous recognition on either session stopped or canceled events
+    speech_recognizer.session_stopped.connect(stop_cb)
+    speech_recognizer.canceled.connect(stop_cb)
+
+    # Start continuous speech recognition
+    speech_recognizer.start_continuous_recognition()
+    while not done:
+        time.sleep(.5)
+
+    speech_recognizer.stop_continuous_recognition()
+    # </SpeechContinuousRecognitionWithFile>
+
+# Creates an instance of a speech config with specified subscription key and service region.
+# Replace with your own subscription key and region identifier from here: https://aka.ms/speech/sdkregion
+speech_key, service_region = "c6038ecd756a494098f0970fd5d4855f", "westus"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+# Creates an audio configuration that points to an audio file.
+# Replace with your own audio filename.
+audio_filename = "Irish/SemiIntelligibleIrish.wav"
+#files = ["Letterkenny/GoTime.wav"]
+print(f"-------------------------\n{audio_filename}\n-------------------------\n")
+speech_recognize_continuous_from_file(audio_filename)
+print(f"\n-------------------------\n")
